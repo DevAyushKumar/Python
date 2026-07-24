4c17ad2bd2843638dd8db22e6eb6a0e264d319c1
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-24 15:09:13 +0530

    Exceptions

---
+++ Documentation/exceptionalHandellling.py
@@ -0,0 +1,6 @@
+'''Exceptional handeling:
+Exception handeling is the process of responding to unwanted or unexpected events when a computer program runs. Exception handelling deals with these events to avoid the program or system crashing, without this process, exceptions would disrupt the normal operation of program.'''
+
+#Exceptions in python
+'''Python has many built-in exceptions that are raised when your program encounters an error(something wrong in program)
+With these exceptions occur, the python interpreter stops the current process and passes it to the calling process until it is handeled. If not handeled, then program will crash.'''
