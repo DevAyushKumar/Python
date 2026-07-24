7ef4fbb616e0838e3c6ec5a45a700491fee99baf
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-24 23:36:46 +0530

    Variable evironment

---
+++ Documentation/virtualEnvironment.py
@@ -0,0 +1,4 @@
+'''Virtual Environment:
+A virtual environment is a tool used to isolate specific python environments on a single machine, allowing you to work on multiple projects with different dependencies and packages withou conflicts. This can be especially useful when you are working on projects that have conflicting packages that are not compatiable with each other.
+To create a virtual environment in Python, you can use the venv module that comes with python.
+python -m venv myenv '''
