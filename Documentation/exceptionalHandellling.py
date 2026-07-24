f96e90a0c7c0ce3b59dba8534f84660cffda959c
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-24 15:14:38 +0530

    Exceptions full notes

--- Documentation/exceptionalHandellling.py
+++ Documentation/exceptionalHandellling.py
@@ -4,3 +4,12 @@ Exception handeling is the process of responding to unwanted or unexpected event
 #Exceptions in python
 '''Python has many built-in exceptions that are raised when your program encounters an error(something wrong in program)
 With these exceptions occur, the python interpreter stops the current process and passes it to the calling process until it is handeled. If not handeled, then program will crash.'''
+
+#python try except
+'''try..excpet blocks are used in python to handle errors and exceptions. The code in try block runs when there is no error. If the try block catches the error, then the except block is executeed'''
+try:
+    for i in range(1,11):
+        a=i+i
+        print(a)
+except:
+    print("Invalid output")
