0c27ed44e4ae6d8454b4abaa55c3b40bc5449729
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-26 23:57:55 +0530

    Getting started in djago

---
+++ Django/gettingStarted.py
@@ -0,0 +1,14 @@
+'''The first thing in the this is to create a virtual environment and need to activate it. It is not necessary to create a virtaul environment but it's a good practice to create a multiple virtual environment for different projects.
+
+Next we need to install django using the command in command line, pip install django
+We can check the installation using the python -m django --version 
+
+We can check the admin commands in django using the django-admin
+
+Now, we need to start the django project using the django-admin startproject name_of_the_project
+
+use cd to get inside the project and use the python manage.py runserver to start the server
+
+we can view the admin login using the /admin in the server link to get in the admin dashboard
+
+use control + c to close the running server'''
