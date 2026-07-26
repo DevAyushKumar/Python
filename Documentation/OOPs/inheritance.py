b8ebd35e99dc9ab16f64a0366f4ec51ca22d48ce
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-26 23:47:18 +0530

    Inheritance

---
+++ Documentation/OOPs/inheritance.py
@@ -0,0 +1,17 @@
+'''Inheritance in Python:
+When a class derives from another class. The child class will inherit all the public and protected properties and meathods from the parent class. In addition, it can have its own properites and meathods, this is called as inheritance.
+Python inheritance syntax:
+class BaseClass:
+    #Body of base class
+    pass
+    class DerivedClass(BaseClass):
+        #body of derived class
+        pass
+Derived class inherits features from the base class where new features can be added to it. This result in re-usablility of code.
+
+Types of inheritance:
+1. Single inheritance
+2. Multiple inheritance
+3. Multilevel inheritance
+4. Hierarchiel inheritance
+5. Hybrid inheritance'''
