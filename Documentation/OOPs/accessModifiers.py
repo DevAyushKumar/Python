9caea07a58c5df7392cf0eb2254189a4b55ad8e4
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-27 18:37:49 +0530

    Access modifers/specifiers

---
+++ Documentation/OOPs/accessModifiers.py
@@ -0,0 +1,7 @@
+'''Access modifiers and specifiers: 
+Access modifiers and access specifiers in python programming are used to limit the access of class varibales and class meathods outside of class while implementing the conecpts of inheritance 
+
+Types of access specifiers:
+1. Public access modifiers
+2. Private access modifiers 
+3. Protected access modifiers'''
