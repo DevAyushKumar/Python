eb9ee51fd46d6d34908d8e39e87ed67aa47bc8a0
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-29 00:57:36 +0530

    static meathods

---
+++ Documentation/OOPs/staticMeathods.py
@@ -0,0 +1,8 @@
+'''Static meathods:
+Static meathod in Python are meathods that belong to a class rather than instance of the class rather than an instance of the class. They are defined using the @staticmeathod decorator and do not have access to the instance of the class (i.e, self). They are called on the class itself, not on an instance of the class. Static meathods are often used to create utility functions that don't need access to instance data.'''
+class math:
+    @staticmethod
+    def add(a,b):
+        return a+b
+result = math.add(1,2)
+print(result)
