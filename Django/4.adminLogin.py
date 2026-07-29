20717554d85f0c84701aaca83014eeb1e8b483df
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-29 19:09:18 +0530

    admin login

---
+++ Django/4.adminLogin.py
@@ -0,0 +1,9 @@
+'''To make a login for admin use the command in the cmd: python manage.py makemigrations.
+
+Now enter python manage.py migrate
+
+Now enter python manage.py createsuperuser, and press enter, now fill all the details in the required field. 
+
+Now run the server, and go to /admin in the login page enter the user you created at the terminal and also the password.
+
+Here you can check every details regarding the person, give them admin access and remove from admin and also create users and delete users.'''
