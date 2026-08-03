7829a853b73c06d7ec7ca33d9149411bc2a1030b
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-03 23:08:51 +0530

    Somethings

---
+++ practic/views.py
@@ -0,0 +1,39 @@
+import json
+
+def mSkills(skillProfile, requiredSkill):
+    can = {skill.lower() for skill in skillProfile}
+    job_req ={skill.lower() for skill in requiredSkill}
+
+    match = can.intersection(job_req)
+
+    percentage = (len(match) / len(job_req)) * 100
+
+    return {
+        "percent" : percentage(round,2)
+    }
+
+def MJobs():
+    with open("profile.json") as f:
+        skillProfile = json.load(f)
+
+    with open("unique_jobs.json") as f:
+        requiredSkill = json.load(f)
+
+    result = []
+
+    for job in requiredSkill:
+        newl=job.s
+
+        mSkills(
+            skillProfile["skills"],
+            requiredSkill["tagsAndSkills"]
+        )
+
+        result.append({
+            "job title" : job["title"]
+        })
+
+    for res in result:
+        print(res)
+
+MJobs()
