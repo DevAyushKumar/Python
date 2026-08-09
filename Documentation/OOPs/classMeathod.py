ce5f4116918ca62d13c2325fb9c5b147ff76ee02
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-09 21:11:12 +0530

    class meathods

---
+++ Documentation/OOPs/classMeathod.py
@@ -0,0 +1,9 @@
+#Python Class Meathods
+'''Python Class Meathods: An introduction
+-> IN Python, classes are a way to define custom data types that can store data and define functions that can manipulate that data. One type of function that can be defined within a class is called a "meathod". In this blog post, we will explore what python class meatods are, why they are useful and how to use them.
+
+What are python class meathods ?
+-> A class meathod is a type of meathod that is bound to the class not the inheritance of the class. In other words, it operates on the class as a whole, rather than on a specific instance of the class. Class meathods are defined using the "@classmeathod" decorator, followed by a function defination. The first argument of the function is always "cls", which represents the class itself. 
+
+Why use python class meathods ?
+-> Class meathods are useful in several situations. For '''
