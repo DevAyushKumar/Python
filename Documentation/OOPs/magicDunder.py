b5af64b08e48c0f995dd0af42d12db1ef3b21c2a
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-11 19:46:24 +0530

    str and repr

--- Documentation/OOPs/magicDunder.py
+++ Documentation/OOPs/magicDunder.py
@@ -6,4 +6,5 @@ Magic meathods, also known as "dunders" from the double underscores surrounding 
 '''__init__ meathod:
 The init meathod is a special meathod that is automatically invoked when you create a new instance of a class. This meathod is responsible for setting up the object's initial state, and it is where you would typically define any instance variables that you need. Also called "constructor", we have discussed this meathod already. 
 
-'''
+__str__ and __repr__ meathods:
+The str and repr meathods are both used to connect an object to a string representation. The str meathod is used when you want to print out an object, while the repr meathod is used when you want to get a string representation of an object that can be used to recreate the object.'''
