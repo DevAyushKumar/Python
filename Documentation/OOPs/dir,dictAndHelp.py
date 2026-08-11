85a5ee0e74639550f222dd4213d794be726e0245
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-11 17:00:54 +0530

    dir meathod

---
+++ Documentation/OOPs/dir,dictAndHelp.py
@@ -0,0 +1,9 @@
+#dir(), __dict__ and help() meathods in python
+'''We must look into dir(), __dict__ and help() attribute/meathods in python. They make it easy for us to understand how classes resolve various functions and executes code. In python, there are there are built-in functions that are commonly used to get information about objects: dir(), dict and help(). Lets's take a look at each of them'''
+
+'''The dir() meathod:
+dir(): The dir() function returns a list of all the attributes and meathods (including dunder meathods) available for an object. It is a useful tool for discovering what you can do with an object'''
+x = [1,2,3]
+dir(x)
+['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__','__getattribute__','__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__','__init__']
+
