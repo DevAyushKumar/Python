760407d73a4a2bf847d71df2f79ffb380a00b0ad
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-11 18:53:21 +0530

    __init__ meathod

--- Documentation/OOPs/magicDunder.py
+++ Documentation/OOPs/magicDunder.py
@@ -2,3 +2,8 @@
 These are special meathods that you can define in you classes, and when invoked, they give you a powerful way to manipulate objects and behaviour.
 
 Magic meathods, also known as "dunders" from the double underscores surrounding their names, are powerful tools that allow you to customize the behaviour of the classes. The y are used to impolement special meathods such as addition, subtraction and comparison operator, as we all some more advance techniques like descriptors and properties.'''
+
+'''__init__ meathod:
+The init meathod is a special meathod that is automatically invoked when you create a new instance of a class. This meathod is responsible for setting up the object's initial state, and it is where you would typically define any instance variables that you need. Also called "constructor", we have discussed this meathod already. 
+
+'''
