df70652ad00000a5c671c7c4842a3b89a985360b
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-11 18:03:48 +0530

    intro

---
+++ Documentation/OOPs/magicDunder.py
@@ -0,0 +1,4 @@
+'''Magic/Dunder meathods in Python:
+These are special meathods that you can define in you classes, and when invoked, they give you a powerful way to manipulate objects and behaviour.
+
+Magic meathods, also known as "dunders" from the double underscores surrounding their names, are powerful tools that allow you to customize the behaviour of the classes. The y are used to impolement special meathods such as addition, subtraction and comparison operator, as we all some more advance techniques like descriptors and properties.'''
