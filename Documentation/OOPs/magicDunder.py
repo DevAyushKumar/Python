2bcb03fa5106817d5575462e63891c47efcbf6c0
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-12 23:06:37 +0530

    call meathod

--- Documentation/OOPs/magicDunder.py
+++ Documentation/OOPs/magicDunder.py
@@ -6,8 +6,13 @@ Magic meathods, also known as "dunders" from the double underscores surrounding 
 '''__init__ meathod:
 The init meathod is a special meathod that is automatically invoked when you create a new instance of a class. This meathod is responsible for setting up the object's initial state, and it is where you would typically define any instance variables that you need. Also called "constructor", we have discussed this meathod already. 
 
-__str__ and __repr__ meathods:
+__str__ and __repr__ methods:
 The str and repr meathods are both used to connect an object to a string representation. The str meathod is used when you want to print out an object, while the repr meathod is used when you want to get a string representation of an object that can be used to recreate the object.
 
-__len__ meathod:
-The len meathod is used to get the length of an object. This is useful when you want to be able to find the size of a data structure, such as list or dictoinary.'''
+__len__ method:
+The len meathod is used to get the length of an object. This is useful when you want to be able to find the size of a data structure, such as list or dictoinary.
+
+__call__ method:
+The call method is used to make an object callable, meaning that you can pass it as a parameter to a function and it will be executed when the function is called. This is an incredibly powerful tool that allows you to ceate objects that behave like functions.
+
+'''
