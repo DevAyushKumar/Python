13a766748919e25ed36cd8032d2a1bbaa4cdf227
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-14 23:53:18 +0530

    syntax of single inheritance

--- Documentation/OOPs/singleInheritance.py
+++ Documentation/OOPs/singleInheritance.py
@@ -1,4 +1,9 @@
 '''Single Inheritance in Python:
 Single inheritance is a type of inheritance when a class inherits properties and behaviours from a singlr parent class. This is the simplest and most common form of inheritance.
 
-'''
+Synatx:
+The synatx for single inheritance in Python is straightforward and easy to understand. To create a new class that inherits from a parent class, simply specify the parent class in the class defination, inside the parentheses, like this'''
+class Childclass(parentClass):
+    def __init__(self):
+        return pass
+
