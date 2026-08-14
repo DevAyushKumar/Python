5cb306b3c8c39527647235225bee7c5bb5fa202c
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-14 23:50:34 +0530

    single inheritance intro

---
+++ Documentation/OOPs/singleInheritance.py
@@ -0,0 +1,4 @@
+'''Single Inheritance in Python:
+Single inheritance is a type of inheritance when a class inherits properties and behaviours from a singlr parent class. This is the simplest and most common form of inheritance.
+
+'''
