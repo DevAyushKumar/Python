788970e97519516a83354cd89780099ac33abc52
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-15 15:06:09 +0530

    multiple inheritance info and start

---
+++ Documentation/OOPs/multipleInheritance.py
@@ -0,0 +1,9 @@
+'''Multiple Inheritance in Python:
+Multiple inheritance is a powerful feature in object-oriented programming that allows a class to inherit attributes and meathod from multiple parent class. This can be useful in situations when a class needs to inherit functionally from multiple sources.
+
+Syntax:
+In Python, multiple inheritances is implemented by specifying multiple parent in a class defination, seperated by commas.'''
+class childclass(parentClass, ParentClass1,ParentClass2, ParentClasss3):
+    def __init__(self):
+        pass
+'''In this example, the ChildClass inherits attributes and meathods from all these parent classess: ParentClass, ParentClass1, ParentClass2 and parentClass3.'''
