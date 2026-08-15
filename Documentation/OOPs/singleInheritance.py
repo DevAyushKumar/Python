73c410c8c3a7c9b7acfcb359a303cbfcf9cacbc6
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-15 13:39:55 +0530

    example

--- Documentation/OOPs/singleInheritance.py
+++ Documentation/OOPs/singleInheritance.py
@@ -7,3 +7,22 @@ class Childclass(parentClass):
     def __init__(self):
         pass
 
+'''Example:
+Let's consider a single experience of single inheritance in Python. Consider a class named "Animal" that contains the attributes and behanviours that are common in animals.'''
+class animals:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def make_sound(self):
+        print("Sound made by animals")
+
+'''If we want to create a new class for a specific type of animal, such as a dog, we can create a new class named "dog" that inherits from the animal class.'''
+class Dog(animals):
+    def __init__(self, name, breed):
+        animals.__init__(self, name, species="dog")
+        self.breed = breed
+    
+    def make_sound(self):
+        print("bark!")
+
