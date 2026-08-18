75d05e73a5d2369fb0486975cfde75d4cc7b3009
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-18 19:04:39 +0530

    recursion

--- DSA/DSA testing/recursion.py
+++ DSA/DSA testing/recursion.py
@@ -1,23 +1,7 @@
 def rec(num):
-    if num>2:
-        return  rec(num-1) + rec(num-2)
+    if (num > 1):
+        return rec(num - 1) + rec(num - 2)
     else:
         return 1
 
-print(rec(6))
-
-def rece(nume):
-    if nume>1:
-        return nume * rece(nume-1)
-    else:
-        return 1
-
-print(rece(4))
-
-def recurring(number):
-    if number > 1:
-        return number * recurring(number - 1)
-    else:
-        return 1
-
-print(recurring(4))
+print(rec(5))
