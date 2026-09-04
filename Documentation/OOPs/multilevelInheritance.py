7adbf789442c0a74e85d7fc80a190cbcf8e12482
DevAyushKumar <blaze.main.id@gmail.com>
2026-09-04 23:54:55 +0530

    changes

--- Documentation/OOPs/multilevelInheritance.py
+++ Documentation/OOPs/multilevelInheritance.py
@@ -1,4 +1,6 @@
 '''Multilevel Inheritance in Python
 Multilevel inheritance is a type of inheritance in object-oriented programming where a derived class inherits from another class. This type of inheritance allows you to build a hierarchy of classes where one class builds upon another, leading to a ore specialized class.
 
-In Python, multilevel inheritance is achieved using the class hierarchy. The syntax for multilevel inheritance is quite simple and follows the same syntax as a single inheritance'''
+In Python, multilevel inheritance is achieved using the class hierarchy. The syntax for multilevel inheritance is quite simple and follows the same syntax as a single inheritance
+
+'''
